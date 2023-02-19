97abeb81cf2607f78c15142010be896a94fbfdae
dariatarakanova <dshargalina@gmail.com>
2023-02-19 14:39:47 +0300

    Bug fix

--- app/src/main/java/hexlet/code/BasicFiles.java
+++ app/src/main/java/hexlet/code/BasicFiles.java
@@ -31,9 +31,9 @@ public class BasicFiles {
                 "}";
         byte[] bs1 = textForFile1.getBytes();
         Path writtenFilePath1 = Files.write(filepath1, bs1);
-        String content1 = Files.readString(writtenFilePath1);
-        System.out.println(content1);
-        System.out.println();
+        //String content1 = Files.readString(writtenFilePath1);
+        //System.out.println(content1);
+        //System.out.println();
 
         String textForFile2 = "{\n" +
                 "  \"timeout\": 20,\n" +
@@ -42,8 +42,8 @@ public class BasicFiles {
                 "}";
         byte[] bs2 = textForFile2.getBytes();
         Path writtenFilePath2 = Files.write(filepath2, bs2);
-        String content2 = Files.readString(writtenFilePath2);
-        System.out.println(content2);
+        //String content2 = Files.readString(writtenFilePath2);
+        //System.out.println(content2);
 
         } catch (Exception e) {
             e.printStackTrace();
