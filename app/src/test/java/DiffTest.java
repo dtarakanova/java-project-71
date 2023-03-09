2d6dc7504909e05c872bf94f6efff7cf46d1d5fd
dariatarakanova <dshargalina@gmail.com>
2023-03-09 16:58:35 +0300

    Add tests of plain comparison

---
+++ app/src/test/java/DiffTest.java
@@ -0,0 +1,43 @@
+import hexlet.code.Diff;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DiffTest {
+    public static final String FILE1_JSON_FILEPATH1 =
+            "./src/test/resources/JSON-files/file1J.json";
+    public static final String FILE2_JSON_FILEPATH2 =
+            "./src/test/resources/JSON-files/file2J.json";
+
+    public static final String EMPTYFILE_JSON_PATH =
+            "./src/test/resources/JSON-files/emptyfile.json";
+    private static final String DEFAULT_CORRECT_RESULT = "{\n"
+            + "- age : 18"
+            + "+ faculty : computer science"
+            + "- internship : false"
+            + "+ internship : true"
+            + "  lastname: petrova"
+            + "  name: sasha"
+            + "- student : true"
+            + "}";
+    private static final String EMPTY_FILE_COMPARISON_RESULT = "{\n"
+            + "+ age : 18"
+            + "+ internship : false"
+            + "+ lastname: petrova"
+            + "+ name: sasha"
+            + "+ student : true"
+            + "}";
+
+
+    @Test
+        public void testRightComparison() throws Exception {
+        String result = Diff.generate(FILE1_JSON_FILEPATH1, FILE2_JSON_FILEPATH2);
+            assertEquals(result, DEFAULT_CORRECT_RESULT);
+        }
+
+        @Test
+        public void emptyFile() throws Exception {
+            String result = Diff.generate(FILE1_JSON_FILEPATH1, EMPTYFILE_JSON_PATH);
+            assertEquals(result, EMPTY_FILE_COMPARISON_RESULT);
+        }
+}
